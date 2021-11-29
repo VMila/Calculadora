dd1c8c657e93355a161d3b0a086a955d2545beab
VMila <vi-milanez@hotmail.com>
2021-11-29 16:05:30 -0300

    calculadora feita patrao

---
+++ script.js
@@ -0,0 +1,81 @@
+const calculadora = document.querySelector('.calculadora')
+const teclas = document.querySelector('.teclas')
+const visor = document.querySelector('.visor')
+
+teclas.addEventListener('click', (evento) => {
+if(!evento.target.closest('button')) return
+
+    const tecla = evento.target
+    const valorTecla = tecla.textContent
+    const { tipoTecla } = tecla.dataset
+    const { tipoTeclaAnterior, memoria } = calculadora.dataset
+    const valorVisor = visor.textContent
+
+    if(tipoTecla === 'numero'){
+
+    if(valorVisor === '0' || tipoTeclaAnterior === 'operador') {
+        if(valorTecla === '00'){
+         visor.textContent = '0'    
+        } else{
+      visor.textContent = valorTecla
+        }
+    }
+    else {
+        visor.textContent = valorVisor + valorTecla
+    }
+}
+
+   if(tipoTecla === 'operador'){
+    const operadores = teclas.querySelectorAll('[data-tipo-tecla="operador"]')
+    operadores.forEach((operador) => {
+    operador.dataset.estado = ''    
+    })
+    tecla.dataset.estado = 'selecionado'
+
+    calculadora.dataset.primeiroNum = valorVisor
+    calculadora.dataset.operador = tecla.dataset.tecla
+   }   
+
+   if(tipoTecla === 'igual') {
+    const primeiroNum = parseFloat(calculadora.dataset.primeiroNum)
+    const operador = calculadora.dataset.operador
+    const segundoNum = parseFloat(valorVisor)
+ 
+    let resultado = ''
+    if(operador == 'somar'){
+    resultado = primeiroNum + segundoNum    
+    }
+    if(operador == 'subtrair'){
+        resultado = primeiroNum - segundoNum    
+        }
+    if(operador == 'multiplicar'){
+    resultado = primeiroNum * segundoNum    
+    }
+    if(operador == 'dividir'){
+    resultado = primeiroNum / segundoNum    
+    }
+    visor.textContent = resultado
+   }
+
+   if(tipoTecla === 'limpar'){
+   visor.textContent = '0'    
+   }
+
+    if(tipoTecla === 'salvar'){
+    calculadora.dataset.memoria = valorVisor
+    }
+   
+    if(tipoTecla === 'recuperar'){
+    if(memoria === undefined){
+        visor.textContent = '0'
+    } else{
+    visor.textContent = memoria
+    }
+    }
+    calculadora.dataset.tipoTeclaAnterior = tipoTecla
+}
+)
+
+
+
+
